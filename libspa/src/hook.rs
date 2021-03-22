c2f14a896b737543033c2404f0ba9d6021e6add7
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2021-03-22 17:03:05 +0100

    spa: hook: document all the API

--- libspa/src/hook.rs
+++ libspa/src/hook.rs
@@ -1,8 +1,11 @@
 // Copyright The pipewire-rs Contributors.
 // SPDX-License-Identifier: MIT
 
+//! SPA hook
+
 use crate::list;
 
+/// Remove a hook
 pub fn remove(mut hook: spa_sys::spa_hook) {
     list::remove(&hook.link);
 
