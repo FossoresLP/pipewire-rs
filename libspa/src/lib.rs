f44943127539789833628a5a58189d18dbd2a34f
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2021-03-22 16:47:03 +0100

    spa: warn on missing docs

--- libspa/src/lib.rs
+++ libspa/src/lib.rs
@@ -1,6 +1,11 @@
 // Copyright The pipewire-rs Contributors.
 // SPDX-License-Identifier: MIT
 
+#![warn(missing_docs)]
+
+//! The `libspa` crate provides a high-level API to interact with
+//! [libspa](https://gitlab.freedesktop.org/pipewire/pipewire/-/tree/master/doc/spa).
+
 pub mod dict;
 pub use dict::*;
 pub mod result;
