4890c642b121a07ac50c3facba527989150378f6
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2021-03-22 17:03:05 +0100

    spa: list: document all the API

--- libspa/src/list.rs
+++ libspa/src/list.rs
@@ -1,6 +1,9 @@
 // Copyright The pipewire-rs Contributors.
 // SPDX-License-Identifier: MIT
 
+//! SPA list
+
+/// Remove an element from its list
 pub fn remove(elem: &spa_sys::spa_list) {
     unsafe {
         (*elem.prev).next = elem.next;
